04036619482bf32947e6c84672f2181c69097483
Kyle <me@kylekeenan.co.uk>
2022-10-10 15:05:42 +0100

    Initial console app creation

---
+++ RubiksCubeTTC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubiksCubeTTC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = "";
+
+            while (true)
+            {
+                userInput = Console.Read().ToString();
+                if()
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
